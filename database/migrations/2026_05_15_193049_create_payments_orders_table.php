9d03c2c5895a9518dc7e6f9b68b6b055baaca50f
Henrique R. <konac@oulook.com.br>
2026-05-15 16:40:01 -0300

    init

---
+++ database/migrations/2026_05_15_193049_create_payments_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments_orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreign('client_id')->constrained('clients');
+            
+            $table->enum('status', ['pending', 'paid', 'cancelled']);
+            $table->decimal('value', 10, 2);
+            $table->date('due_date');
+            $table->date('payment_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments_orders');
+    }
+};
